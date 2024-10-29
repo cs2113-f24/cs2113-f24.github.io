d6262f793c16c9687568a20f010f8a3186fefa85
Kinga <kinga@gwu.edu>
2024-10-29 07:48:47 -0400

    lab3

--- labs/Lab3_Tester.java
+++ labs/Lab3_Tester.java
@@ -9,6 +9,11 @@ import java.io.InputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.FileOutputStream;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 
 public class Lab3_Tester {
 
